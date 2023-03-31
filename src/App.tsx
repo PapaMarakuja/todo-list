77ba1aec47fff3fe57b420d3620d753c188f6cdd
Rafael Pereira <rafael.p.24.03@gmail.com>
2023-03-31 11:45:07 -0300

    base

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const list = [
+    {
+      title: 'Anything',
+      content: 'blá blá blá',
+    },
+    {
+      title: 'Other thing',
+      content: 'sadkfjasdjkfhjkadsfkaskjhasifhikujas',
+    },
+  ];
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-slate-100 text-zinc-800 h-screen flex justify-center'>
+      <div className='container px-4'>
+        <h1 className='text-3xl text-center font-bold mb-10 mt-4'>Rafael's Todo List</h1>
+
+        <div className='grid place-items-center mb-4'>
+          <input
+            className='w-full border rounded-2xl px-4 py-2'
+            type='text'
+            placeholder='Search'
+          />
+          <div className='flex gap-4 pt-2'>
+            <label>
+              <input type='checkbox' /> Done
+            </label>
+            <label>
+              <input type='checkbox' /> Pending
+            </label>
+          </div>
+        </div>
+
+        <div className='grid grid-cols-2 gap-2'>
+          {list.map((things: TodoList) => (
+            <div className='border border-cyan-700 rounded-lg px-4 py-2'>
+              <h1 className='mb-2'>{things.title}</h1>
+              <p>{things.content}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+interface TodoList {
+  title: string;
+  content: string;
+}
+
+export default App;
