8bc4a27becadcfb6d849c876cb209afb953e7d9c
Rafael Pereira <rafael.p.24.03@gmail.com>
2023-04-03 15:01:46 -0300

    list changes | 90%

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,94 @@
+import { useState } from 'react';
 import './App.css';
+import { CheckIcon, PlusIcon, XMarkIcon } from './assets/icons';
 
 function App() {
-  const list = [
+  const [newItem, setNewItem] = useState<string>();
+  const [list, setList] = useState<TodoList[]>([
     {
       title: 'Anything',
-      content: 'blá blá blá',
+      done: false,
     },
     {
       title: 'Other thing',
-      content: 'sadkfjasdjkfhjkadsfkaskjhasifhikujas',
+      done: true,
     },
-  ];
-  return (
-    <div className='bg-slate-100 text-zinc-800 h-screen flex justify-center'>
-      <div className='container px-4'>
-        <h1 className='text-3xl text-center font-bold mb-10 mt-4'>Rafael's Todo List</h1>
+  ]);
 
-        <div className='grid place-items-center mb-4'>
-          <input
-            className='w-full border rounded-2xl px-4 py-2'
-            type='text'
-            placeholder='Search'
-          />
-          <div className='flex gap-4 pt-2'>
-            <label>
-              <input type='checkbox' /> Done
-            </label>
-            <label>
-              <input type='checkbox' /> Pending
-            </label>
+  function addItem(title: any) {
+    if (!title) {
+      return;
+    }
+
+    setList([...list, { title: title, done: false }]);
+    setNewItem('');
+  }
+
+  function markAsChecked(index: number) {
+    const newList = [...list];
+    newList[index].done = true;
+    setList(newList);
+  }
+
+  function deleteItem(index: number) {
+    setList(list.filter((_, i) => i !== index));
+  }
+
+  function handleNewItemChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setNewItem(event.target.value);
+  }
+
+  return (
+    <div className='gradient text-zinc-800 h-screen flex justify-center'>
+      <div className='container flex items-center flex-col px-4 gap-4'>
+        <h1 className='text-3xl text-center font-bold my-4'>Rafael's Todo List</h1>
+
+        <div className='w-full max-w-4xl bg-gray-100 bg-opacity-70 p-4 rounded-xl backdrop-blur-lg backdrop-saturate-150'>
+          <div className='grid place-items-center'>
+            <input
+              className='w-full rounded-2xl px-4 py-2'
+              type='text'
+              placeholder='Add new'
+              value={newItem}
+              onChange={handleNewItemChange}
+            />
+            <button onClick={() => addItem(newItem)} className='absolute top-6 right-8'>
+              <PlusIcon />
+            </button>
+            <div className='flex gap-4 pt-2'>
+              <label>
+                <input type='checkbox' /> Done
+              </label>
+              <label>
+                <input type='checkbox' /> Pending
+              </label>
+            </div>
           </div>
         </div>
 
-        <div className='grid grid-cols-2 gap-2'>
-          {list.map((things: TodoList) => (
-            <div className='border border-cyan-700 rounded-lg px-4 py-2'>
-              <h1 className='mb-2'>{things.title}</h1>
-              <p>{things.content}</p>
-            </div>
-          ))}
+        <div className='w-full max-w-4xl bg-gray-100 bg-opacity-70 p-4 rounded-xl backdrop-blur-lg backdrop-saturate-150'>
+          <ul className='flex flex-col gap-2'>
+            {list.map((thing: TodoList, index: number) => (
+              <li
+                key={index}
+                className={`bg-slate-400 bg-opacity-75 rounded-md px-4 py-2 flex justify-between break-words ${
+                  thing.done ? 'bg-slate-800 text-slate-400 line-through' : ''
+                }`}
+              >
+                <h1>{thing.title}</h1>
+                <div className='flex gap-2 justify-start'>
+                  {!thing.done && (
+                    <button onClick={() => markAsChecked(index)}>
+                      <CheckIcon />
+                    </button>
+                  )}
+                  <button onClick={() => deleteItem(index)}>
+                    <XMarkIcon />
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
@@ -47,7 +97,7 @@ function App() {
 
 interface TodoList {
   title: string;
-  content: string;
+  done: boolean;
 }
 
 export default App;
---
+++ src/assets/icons.tsx
@@ -0,0 +1,44 @@
+export function XMarkIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className='w-6 h-6'
+    >
+      <path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
+    </svg>
+  );
+}
+
+export function CheckIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className='w-6 h-6'
+    >
+      <path strokeLinecap='round' strokeLinejoin='round' d='M4.5 12.75l6 6 9-13.5' />
+    </svg>
+  );
+}
+
+export function PlusIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className='w-6 h-6'
+    >
+      <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
+    </svg>
+  );
+}
