aa6d6321065f29f14b51ea6186313e29ca233b71
Rafael Pereira <rafael.p.24.03@gmail.com>
2023-04-04 17:26:19 -0300

    lottie

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,7 +1,16 @@
+import Lottie from 'react-lottie-player';
+import animationData from '../assets/lootie-not-found.json';
+
 function NotFound() {
   return (
-    <div className='text-center'>
-      <h1>It seems you have nothing to show here!</h1>
+    <div className='grid place-items-center'>
+      <Lottie
+        loop
+        animationData={animationData}
+        play
+        style={{ width: 150, height: 150 }}
+      />
+      <span>It seems you have nothing to show here!</span>
     </div>
   );
 }
